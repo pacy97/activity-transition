b8cc92679954b2d4e483561aaf92e77467479e46
Pacy <40542052+pacy97@users.noreply.github.com>
2018-07-08 14:47:39 +0530

    Update ChangeActivityMain.java

--- ChangeActivityMain.java
+++ ChangeActivityMain.java
@@ -1,27 +1,36 @@
-package com.example.Pacasian.pacy;
+package in.pacasian.sumithkumar.q;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
 
-/**
- * Created by sumith on 16-10-2017.
- */
-
-public class about extends AppCompatActivity {
-
-private Button button;
-
+public class ChangeActivityMain extends AppCompatActivity {
+    EditText ed1;
+    Button btn;
+    String string;
     @Override
-    protected void onCreate( Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.about);
-        button=(TextView)findViewById(R.id.btn);
-        button.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_simple_camera);
+        btn=(Button)findViewById(R.id.btn);
+        ed1=(EditText)findViewById(R.id.num);
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-               //Add your operation here.......
+            public void onClick(View v) {
+                startActivity(new Intent(SimpleCamera.this,second.class));
             }
-        });
-    }
+    });
+}
 }
